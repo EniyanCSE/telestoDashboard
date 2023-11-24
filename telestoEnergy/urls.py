6d17c93bbedfd9cd022f622cfde7ff7834b42378
RajKumar-Developer <rajkumarrcse28@gmail.com>
2023-11-24 14:34:12 +0530

    adding the existing project

--- telestoEnergy/urls.py
+++ telestoEnergy/urls.py
@@ -14,12 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('',include('telestoapp.urls')),
-    path('myaccount/',include('telestoapp.urls')),
     path("admin/", admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^(?P<path>.+\..+)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
--- telestoapp/views.py
+++ telestoapp/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 # Create your views here.
 
 def index(request):
@@ -7,3 +6,51 @@ def index(request):
 
 def pages_profile(request):
     return render(request, 'telestoapp/pages-profile.html')
+
+def Hi_Analysis(request):
+    return render(request,'telestoapp/Hi_Analysis.html')
+
+def Adca(request):
+    return render(request,'telestoapp/ADCA.html')
+
+def Mat_Balance(request):
+    return render(request,'telestoapp/Mat_Balance.html')
+
+def flowUnit(request):
+    return render(request,'telestoapp/flowUnit.html')
+
+def FractionalFlow(request):
+    return render(request, 'telestoapp/FractionalFlow.html')
+
+def plots(request):
+    return render(request,'telestoapp/plots.html')
+
+def plotshis(request):
+    return render(request,'telestoapp/plots2.html')
+
+def contact(request):
+    return render(request,'telestoapp/contact.html')
+
+def SatMap(request):
+    return render(request,'telestoapp/SatMap.html')
+
+def mL_model(request):
+    return render(request,'telestoapp/ML_model.html')
+
+def pages_profile(request):
+    return render(request,'telestoapp/pages-profile.html')
+
+def pages_faq(request):
+    return render(request,'telestoapp/pages-faq.html')
+
+def auth_lock_screen(request):
+    return render(request,'telestoapp/auth-lock-screen.html')
+
+def auth_logout_2(request):
+    return render(request,'telestoapp/auth-logout-2.html')
+
+def auth_login(request):
+    return render(request,'telestoapp/auth-login-2.html')
+
+def auth_register_2(request):
+    return render(request,'telestoapp/auth-register-2.html')
