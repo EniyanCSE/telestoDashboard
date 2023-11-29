2899610a1ac224e49b8807fde36bcd5d3e60d9f5
RajKumar-Developer <rajkumarrcse28@gmail.com>
2023-11-29 23:07:23 +0530

    done with new satmap and vrr analysis graph

--- telestoapp/static/vrrAnalysis/vrrScript.js
+++ telestoapp/static/vrrAnalysis/vrrScript.js
@@ -1,75 +1,3 @@
-// d3.csv("VRR_Analysis.csv", function (d) {
-//   return {
-//     date: new Date(d.Date),
-//     instantaneousVRR: +d["Instantaneous VRR"],
-//     cumulativeVRR: +d["Cummulative VRR"],
-//   };
-// })
-//   .then(function (data) {
-//     // Separate the data into x, y1, and y2
-//     let xValues = data.map((d) => d.date);
-//     let yValuesInstantaneous = data.map((d) => d.instantaneousVRR);
-//     let yValuesCumulative = data.map((d) => d.cumulativeVRR);
-
-//     // Plot the data using Plotly
-//     let trace1 = {
-//       x: xValues,
-//       y: yValuesInstantaneous,
-//       name: "Instantaneous VRR",
-//       type: "scatter",
-//       yaxis: "y",
-//       line: { color: "lightblue" }, // Light blue color for Instantaneous VRR
-//     };
-
-//     let trace2 = {
-//       x: xValues,
-//       y: yValuesCumulative,
-//       name: "Cumulative VRR",
-//       type: "scatter",
-//       yaxis: "y2",
-//       line: { color: "darkblue" }, // Dark blue color for Cumulative VRR
-//     };
-
-//     let layout = {
-//       title: "VRR Analysis",
-//       xaxis: {
-//         title: "Date",
-//         rangeselector: {
-//           buttons: [
-//             {
-//               count: 1,
-//               label: "1m",
-//               step: "month",
-//               stepmode: "backward",
-//             },
-//             {
-//               count: 6,
-//               label: "6m",
-//               step: "month",
-//               stepmode: "backward",
-//             },
-//             { step: "all" },
-//           ],
-//         },
-//         rangeslider: { range: [d3.min(xValues), d3.max(xValues)] },
-//         type: "date",
-//       },
-//       yaxis: { title: "Instantaneous VRR" },
-//       yaxis2: {
-//         title: "Cumulative VRR",
-//         overlaying: "y",
-//         side: "right",
-//       },
-//       width: 800,
-//       height: 600,
-//     };
-
-//     Plotly.newPlot("gd", [trace1, trace2], layout);
-//   })
-//   .catch(function (error) {
-//     console.error("Error loading CSV: ", error);
-//   });
-
 d3.csv("VRR_Analysis.csv", function (d) {
   return {
     date: new Date(d.Date),
@@ -90,7 +18,7 @@ d3.csv("VRR_Analysis.csv", function (d) {
       name: "Instantaneous VRR",
       type: "scatter",
       yaxis: "y",
-      line: { color: "lightblue" }, // Light blue color for Instantaneous VRR
+      line: { color: "lightblue" },
     };
 
     let trace2 = {
@@ -99,33 +27,42 @@ d3.csv("VRR_Analysis.csv", function (d) {
       name: "Cumulative VRR",
       type: "scatter",
       yaxis: "y2",
-      line: { color: "darkblue" }, // Dark blue color for Cumulative VRR
+      line: { color: "darkblue" },
     };
 
     let layout = {
-      title: "VRR Analysis",
-      autosize: true,
+      title: "VRR Analysis - Well HJN045",
       xaxis: {
-        title: "Date",
+        title: "",
         rangeselector: {
           buttons: [
             {
               count: 1,
-              label: "1m",
-              step: "month",
+              label: "1y",
+              step: "year",
               stepmode: "backward",
             },
             {
-              count: 6,
-              label: "6m",
-              step: "month",
+              count: 3,
+              label: "3y",
+              step: "year",
+              stepmode: "backward",
+            },
+            {
+              count: 5,
+              label: "5y",
+              step: "year",
+              stepmode: "backward",
+            },
+            {
+              count: 10,
+              label: "10y",
+              step: "year",
               stepmode: "backward",
             },
             { step: "all" },
           ],
         },
-        rangeslider: { range: [d3.min(xValues), d3.max(xValues)] },
-        type: "date",
       },
       yaxis: { title: "Instantaneous VRR" },
       yaxis2: {
@@ -138,10 +75,32 @@ d3.csv("VRR_Analysis.csv", function (d) {
     };
 
     Plotly.newPlot("gd", [trace1, trace2], layout);
-    windows.onresize = function () {
-      Plotly.relayout("gd", {});
-    };
   })
   .catch(function (error) {
     console.error("Error loading CSV: ", error);
   });
+var slider = document.getElementById("yearSlider");
+var output = document.getElementById("sliderValue");
+var baseYear = 1995; // Starting year
+var stepSize = 5; // Increment size in years
+
+// Function to calculate the year based on the slider value
+function calculateYear(sliderValue) {
+  return baseYear + sliderValue * stepSize;
+}
+
+// Initialize slider display
+output.innerHTML = calculateYear(slider.value);
+
+// Update when the slider is moved
+slider.oninput = function () {
+  var selectedYear = calculateYear(this.value);
+  output.innerHTML = selectedYear;
+  let startDate = new Date(selectedYear, 0, 1);
+  let endDate = new Date(selectedYear + stepSize, 0, 1);
+
+  let update = {
+    "xaxis.range": [startDate, endDate],
+  };
+  Plotly.relayout("gd", update);
+};
