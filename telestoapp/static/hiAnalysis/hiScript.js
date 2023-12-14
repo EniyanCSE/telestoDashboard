a81b9b4a65f4f758578fad05340e6b54e21e8684
RajKumar-Developer <rajkumarrcse28@gmail.com>
2023-12-14 10:10:24 +0530

    updated satmap

--- telestoapp/static/hiAnalysis/hiScript.js
+++ telestoapp/static/hiAnalysis/hiScript.js
@@ -1,56 +1,58 @@
-document
-  .getElementById("csv-file")
-  .addEventListener("change", function (event) {
-    var file = event.target.files[0];
-    var reader = new FileReader();
+var allData = [];
+var layout = {
+  title: "Heterogeneity Index Location Analysis",
+  xaxis: { title: "Hi Water Production" },
+  yaxis: { title: "Hi Oil Production" },
+  showlegend: true,
+};
 
-    reader.onload = function (e) {
-      var text = e.target.result;
-      var data = Plotly.d3.csv.parse(text);
+// Load CSV data from a file path
+var csvFilePath = "Heterogenity-Index.csv";
+console.log("running...");
+var xhr = new XMLHttpRequest();
+xhr.open("GET", csvFilePath, true);
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    var csvData = xhr.responseText;
+    allData = Plotly.d3.csv.parse(csvData);
+    plotData();
+  }
+};
+xhr.send();
 
-      var traces = [];
+function plotData() {
+  var traces = [];
+  var classes = [...new Set(allData.map((item) => item["Class 1"]))];
+  var markers = {
+    "High Oil and High Water": "triangle-up",
+    "High Oil and Low Water": "diamond",
+    "Low Oil and High Water": "square",
+    "Low Oil and Low Water": "circle",
+  };
+  var colors = {
+    "High Oil and High Water": "red",
+    "High Oil and Low Water": "green",
+    "Low Oil and High Water": "blue",
+    "Low Oil and Low Water": "magenta",
+  };
 
-      var classes = [...new Set(data.map((item) => item["Class 1"]))];
-      var markers = {
-        "High Oil and High Water": "triangle-up",
-        "High Oil and Low Water": "diamond",
-        "Low Oil and High Water": "square",
-        "Low Oil and Low Water": "circle",
-      };
-      var colors = {
-        "High Oil and High Water": "red",
-        "High Oil and Low Water": "green",
-        "Low Oil and High Water": "blue",
-        "Low Oil and Low Water": "magenta",
-      };
-
-      classes.forEach((cls) => {
-        traces.push({
-          x: data
-            .filter((item) => item["Class 1"] === cls)
-            .map((item) => item.X),
-          y: data
-            .filter((item) => item["Class 1"] === cls)
-            .map((item) => item.Y),
-          mode: "markers",
-          type: "scatter",
-          name: cls,
-          marker: {
-            symbol: markers[cls],
-            color: colors[cls],
-          },
-        });
-      });
-
-      var layout = {
-        title: "Heterogeneity Index Location Analysis",
-        xaxis: { title: "Hi Water Production" },
-        yaxis: { title: "Hi Oil Production" },
-        showlegend: true,
-      };
-
-      Plotly.newPlot("myDiv", traces, layout);
-    };
-
-    reader.readAsText(file);
+  classes.forEach((cls) => {
+    traces.push({
+      x: allData
+        .filter((item) => item["Class 1"] === cls)
+        .map((item) => Number(item.X)),
+      y: allData
+        .filter((item) => item["Class 1"] === cls)
+        .map((item) => Number(item.Y)),
+      mode: "markers",
+      type: "scatter",
+      name: cls,
+      marker: {
+        symbol: markers[cls],
+        color: colors[cls],
+      },
+    });
   });
+
+  Plotly.newPlot("myDiv", traces, layout);
+}
